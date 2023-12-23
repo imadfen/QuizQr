8f476fb34f0b8c3e63433fa294f2930aabf4212d
imadfen <imadfen20@gmail.com>
2023-12-23 22:52:12 +0100

    edited style of instructions

--- client/src/components/InstuctionsSection.tsx
+++ client/src/components/InstuctionsSection.tsx
@@ -21,16 +21,16 @@ export default function InstuctionsSection() {
     <div className="p-12 bg-purple-950 text-yellow-500 rounded-2xl drop-shadow-2xl">
       <h1 className="text-6xl font-extrabold mb-10">How to play ?</h1>
       <ol className="flex flex-col gap-3 text-2xl font-bold">
-        <li id="step-0" className="duration-200">
+        <li id="step-0" className="duration-200 w-fit">
           1- Scan the given Qr code.
         </li>
-        <li id="step-1" className="duration-200">
+        <li id="step-1" className="duration-200 w-fit">
           2- Follow the link you got on scan.
         </li>
-        <li id="step-2" className="duration-200">
+        <li id="step-2" className="duration-200 w-fit">
           3- Insert your name.
         </li>
-        <li id="step-3" className="duration-200">
+        <li id="step-3" className="duration-200 w-fit">
           4- Have fun.
         </li>
       </ol>
